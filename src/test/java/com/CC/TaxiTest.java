5097bb6b087a1cee9a66165ecd203a8089c28727
Lingyu Zhang <dz21330043@smail.nju.edu.cn>
2022-11-17 16:47:12 +0800

    GEAS ready to test

--- src/test/java/com/CC/TaxiTest.java
+++ src/test/java/com/CC/TaxiTest.java
@@ -4,17 +4,30 @@ import org.junit.jupiter.api.Test;
 
 public class TaxiTest {
 
+//    @Test
+//    void PCC_IMD_CG_Test(){CLIParserTest.testTaxi("PCC+IMD", false);}
+//    @Test
+//    void PCC_IMD_MG_Test(){CLIParserTest.testTaxi("PCC+IMD", true);}
+//    @Test
+//    void ConC_IMD_CG_Test(){CLIParserTest.testTaxi("ConC+IMD", false);}
+//    @Test
+//    void ConC_IMD_MG_Test(){CLIParserTest.testTaxi("ConC+IMD", true);}
+//    @Test
+//    void ECC_IMD_CG_Test(){CLIParserTest.testTaxi("ECC+IMD", false);}
+//    @Test
+//    void ECC_IMD_MG_Test(){CLIParserTest.testTaxi("ECC+IMD", true);}
+
     @Test
-    void PCC_IMD_CG_Test(){CLIParserTest.testTaxi("PCC+IMD", false);}
+    void PCC_GEAS_CG_Test(){CLIParserTest.testTaxi("PCC+GEAS_ori", false);}
     @Test
-    void PCC_IMD_MG_Test(){CLIParserTest.testTaxi("PCC+IMD", true);}
+    void PCC_GEAS_MG_Test(){CLIParserTest.testTaxi("PCC+GEAS_ori", true);}
     @Test
-    void ConC_IMD_CG_Test(){CLIParserTest.testTaxi("ConC+IMD", false);}
+    void ConC_GEAS_CG_Test(){CLIParserTest.testTaxi("ConC+GEAS_ori", false);}
     @Test
-    void ConC_IMD_MG_Test(){CLIParserTest.testTaxi("ConC+IMD", true);}
+    void ConC_GEAS_MG_Test(){CLIParserTest.testTaxi("ConC+GEAS_ori", true);}
     @Test
-    void ECC_IMD_CG_Test(){CLIParserTest.testTaxi("ECC+IMD", false);}
+    void ECC_GEAS_CG_Test(){CLIParserTest.testTaxi("ECC+GEAS_ori", false);}
     @Test
-    void ECC_IMD_MG_Test(){CLIParserTest.testTaxi("ECC+IMD", true);}
+    void ECC_GEAS_MG_Test(){CLIParserTest.testTaxi("ECC+GEAS_ori", true);}
 
 }
