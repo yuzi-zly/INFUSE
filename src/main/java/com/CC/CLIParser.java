a6c400bdada28cbf397d93d32c708f6412266d9c
Lingyu Zhang <171240524@smail.nju.edu.cn>
2023-08-02 19:10:39 +0800

    fix: fix a bug that CLIParser mfunc --\> mfuncs, pattenrs --\> patterns

--- src/main/java/com/CC/CLIParser.java
+++ src/main/java/com/CC/CLIParser.java
@@ -88,7 +88,7 @@ public class CLIParser implements Loggable {
                 .desc("Load patterns from given file (XML file)")
                 .build();
 
-        Option opt_mf = Option.builder("mfunc")
+        Option opt_mf = Option.builder("mfuncs")
                 .argName("file")
                 .hasArg()
                 .required(false)
@@ -341,7 +341,7 @@ java -jar INFUSE.jar
                 System.exit(1);
             }
             else{
-                patternFile = cli.getOptionValue("pattenrs");
+                patternFile = cli.getOptionValue("patterns");
                 logger.info(String.format("The pattern file is \"%s\"", patternFile));
             }
             // mfunc file
